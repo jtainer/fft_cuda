c6f7208bd88b9fc07b5184bdba7f481fc7520122
jtainer <jtainer@github.com>
2023-03-19 01:13:57 -0400

    Added lookup table for FFT twiddle factors

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-nvcc --relocatable-device-code=true -L/usr/local/cuda/lib64 kernel.cu complex_f32.cu -lcudart
+nvcc --relocatable-device-code=true -L/usr/local/cuda/lib64 kernel.cu complex_f32.cu -lcudart -O3
--- kernel.cu
+++ kernel.cu
@@ -11,6 +11,25 @@
 
 #include "complex_f32.h"
 
+// Kernel to calculate values for LUT
+__global__
+void fft_calculate_lut(complex_f32* lut, unsigned int N) {
+	unsigned int tid = threadIdx.x + (blockIdx.x * blockDim.x);
+
+	if (tid < N/2) {
+		lut[tid] = complex_f32(-2*M_PI*tid/N);
+	}
+}
+
+__host__
+complex_f32* fft_gen_device_lut(unsigned int N) {
+	complex_f32* lut;
+	cudaMalloc((void**)&lut, sizeof(complex_f32) * N/2);
+	fft_calculate_lut<<<(N/32) + 1, 32>>>(lut, N);
+	return lut;
+}
+
+
 __host__
 unsigned int count_bits(unsigned int n) {
 	unsigned int bits = 0;
@@ -29,16 +48,6 @@ unsigned int reverse_bits(unsigned int orig, unsigned int bits) {
 	return flip;
 }
 
-// Smallest operation that we can easily parallelize
-__device__
-void fft_combine_single(complex_f32* buf, unsigned int n, unsigned int k) {
-	complex_f32 p(buf[k]);
-	complex_f32 u(-2*M_PI*k/n);
-	complex_f32 q(u * buf[k+n/2]);
-	buf[k] = p + q;
-	buf[k+n/2] = p - q;
-}
-
 // CUDA kernel to rerrange buffer using bit reversal before calculating FFT
 __global__
 void fft_reorder(complex_f32* buf, unsigned int N, unsigned int bits) {
@@ -55,18 +64,30 @@ void fft_reorder(complex_f32* buf, unsigned int N, unsigned int bits) {
 	}
 }
 
+// Smallest operation that we can easily parallelize
+__device__
+void fft_combine_single(complex_f32* buf, complex_f32* lut, unsigned int n, unsigned int k, unsigned int s) {
+	complex_f32 p(buf[k]);
+//	complex_f32 u(-2*M_PI*k/n);
+	complex_f32 u = lut[k*s];
+	complex_f32 q(u * buf[k+n/2]);
+	buf[k] = p + q;
+	buf[k+n/2] = p - q;
+}
+
 // Complete one full iteration of FFT
 // Cant synchronize threads between blocks from inside kernel,
 // need to handle synchronization on CPU instead
 // n = size of sub-FFT
 // N = overall size of FFT
 __global__
-void fft_iteration(complex_f32* buf, unsigned int n, unsigned int N) {
+void fft_iteration(complex_f32* buf, complex_f32* lut, unsigned int n, unsigned int N) {
 	unsigned int tid = threadIdx.x + (blockIdx.x * blockDim.x);
 	if (tid < N/2) {
 		unsigned int w = tid / (n/2);
 		unsigned int k = tid % (n/2);
-		fft_combine_single(buf + w * n, n, k);
+		unsigned int s = N/n;
+		fft_combine_single(buf + w * n, lut, n, k, s);
 	}
 }
 
@@ -74,31 +95,34 @@ void fft_iteration(complex_f32* buf, unsigned int n, unsigned int N) {
 // buf must point to GPU memory
 // N = size of FFT
 __host__
-void fft(complex_f32* buf, unsigned int N) {
+void fft(complex_f32* buf, complex_f32* lut, unsigned int N) {
 	unsigned int bits = count_bits(N);
 	fft_reorder<<<(N/32) + 1, 32>>>(buf, N, bits);
 
 	for (unsigned int n = 2; n <= N; n *= 2) {
-		fft_iteration<<<(N/32) + 1, 32>>>(buf, n, N);
+		fft_iteration<<<(N/32) + 1, 32>>>(buf, lut, n, N);
 	}
 }
-
-
 
 int main() {
 	const unsigned int n = 8;
 	complex_f32 buf[n] = { {1,0}, {2,0}, {5,0}, {2,0}, {7,0}, {4,0}, {2,0}, {0,0} };
 	complex_f32* dbuf;
 	cudaMalloc((void**)&dbuf, sizeof(complex_f32) * n);
+
 	cudaMemcpy(dbuf, buf, sizeof(complex_f32) * n, cudaMemcpyHostToDevice);
-	fft(dbuf, n);
+
+	complex_f32* lut = fft_gen_device_lut(n);
+
+	fft(dbuf, lut, n);
+	
 	cudaMemcpy(buf, dbuf, sizeof(complex_f32) * n, cudaMemcpyDeviceToHost);
 	cudaFree(dbuf);
+	cudaFree(lut);
 
 	for (unsigned int i = 0; i < n; i++) {
 		printf("%f\t%f\n", buf[i].real, buf[i].imag);
 	}
-
 
 	return 0;
 }
